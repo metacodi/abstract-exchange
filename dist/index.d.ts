05bdaefb80e2bfc8ef498dfb5f46508859fee2b7
Jordi Giral <jgiral@metacodi.com>
2022-10-01 16:11:49 +0200

    auto-commit

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,7 @@
 export * from './abstract/abstract-account';
 export * from './abstract/abstract-controller';
 export * from './abstract/abstract-exchange';
+export * from './abstract/task-executor';
 export * from './abstract/orders-executor';
 export * from './abstract/shared';
 export * from './abstract/types';
--- dist/index.js
+++ dist/index.js
@@ -17,6 +17,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./abstract/abstract-account"), exports);
 __exportStar(require("./abstract/abstract-controller"), exports);
 __exportStar(require("./abstract/abstract-exchange"), exports);
+__exportStar(require("./abstract/task-executor"), exports);
 __exportStar(require("./abstract/orders-executor"), exports);
 __exportStar(require("./abstract/shared"), exports);
 __exportStar(require("./abstract/types"), exports);
